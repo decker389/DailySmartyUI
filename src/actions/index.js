4b8fe301c8927057264b660182bd9d1634d2eebe
Mr. Decker <decker389@aol.com>
2021-02-04 15:28:02 -0700

    used axios to perform get request and setup the required reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,17 @@
+import axios from "axios";
+
+import { SET_RECENT_POSTS } from "./types";
+
 export function fetchRecentPosts() {
     return function(dispatch) {
-        console.log('hello');
+        axios.get('https://api.dailysmarty.com/posts')
+        .then(response => {
+            console.log("response", response.data);
+            dispatch({
+                type: SET_RECENT_POSTS,
+                payload: response.data.posts
+            })
+        })
 
     }
 }
