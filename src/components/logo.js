c2abc7d1205e3b99baa6882d50d48845e89c1f8c
Mr. Decker <decker389@aol.com>
2021-02-04 20:13:02 -0700

    modified logo component to take in custom size via props

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,9 +2,13 @@ import React, { Component } from "react";
 
 export default class logo extends Component {
     render() {
+        const size = {
+            height: this.props.size ? this.props.size : 105,
+            width: this.props.size ? this.props.size : 105
+        }
         return (
             <div className="logo-main">
-                <img alt="daily smarty logo image" src="/assets/ds_circle_logo.png" />
+                <img style={size} alt="daily smarty logo image" src="/assets/ds_circle_logo.png" />
             </div>
         )
     }
