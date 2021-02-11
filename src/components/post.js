62e47e115b321bda9417bffc8c67ea1746cd4ccb
christopher decker <decker389@aol.com>
2021-02-11 14:44:40 -0700

    modified post component to render a result post and set up renderLinks function in post file

--- src/components/post.js
+++ src/components/post.js
@@ -9,19 +9,49 @@ class Post extends Component {
         return topics;
     }
 
-    render() {
-        return( 
-            <li className="recent-post">
-                <div className="recent-post-title">
-                    {this.props.title}
-                </div>
+    renderLinks() {
+        let links = this.props.post_links.map((post_link, index) => {
+            return (
+                <div className="post-link" key={index}>
+                <div  className="post-link__box"></div>
 
-                <div className="recent-posts-topics">
-                    {this.renderTopics()}
+                <div className="post-link__link">
+                    <a href={post_link.link_url}>Useful Link #{index + 1}</a>
                 </div>
-                
-            </li>
-        )
+            </div>
+            )
+        })
+        return links;
+    }
+
+    render() {
+        if(this.props.type == "recent") {
+            return (
+                <li className="recent-post">
+                    <div className="recent-post-title">
+                        {this.props.title}
+                    </div>
+
+                    <div className="recent-posts-topics">
+                        {this.renderTopics()}
+                    </div>
+                </li>
+            )
+        } else if (this.props.type === "result") {
+            return (
+                <li className="results-post">
+                    <div className="result-post__topics">
+                        {this.renderTopics()}
+                    </div>
+                    <div className="result-post__title">
+                        {this.props.title}
+                    </div>
+                    <div className="result-post__links">
+                        {this.renderLinks()}
+                    </div>
+                </li>
+            )
+        }
     }
 }
 
