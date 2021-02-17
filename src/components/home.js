2e01aeebc85ab2890434d77d8732d1a0918b084e
christopher decker <decker389@aol.com>
2021-02-16 19:19:02 -0700

    styled searchbar on the results page

--- src/components/home.js
+++ src/components/home.js
@@ -19,7 +19,7 @@ class Home extends Component {
     return (
       <div className='home'>
         <Logo />
-        <Searchbar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
+        <Searchbar page="home" onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
         <RecentPosts />
       </div>
     );
